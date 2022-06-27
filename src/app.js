79018881f4585ea515e07482fa07dfd9ea1ae9d9
dimaredunlap <dimaredunlap@gmail.com>
2022-06-27 23:09:58 +0000

    JS script added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,61 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function deckBuilder() {
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+
+  // Next loop through the suits array whilst also looping through the values array and push the combined data into the cards array:
+
+  const cards = [];
+  for (let s = 0; s < suits.length; s++) {
+    for (let v = 0; v < values.length; v++) {
+      const value = values[v];
+      const suit = suits[s];
+      cards.push({ value, suit });
+    }
+  }
+  return cards;
+}
+// a esta funcion le pasaremos los arrays de cards
+
+function randomCard(cards) {
+  const random = Math.floor(Math.random() * 51);
+  const cardValue = cards[random].value;
+  const cardSuit = cards[random].suit;
+
+  const cardBody =
+    cardSuit === "Diamonds" ? "&diams;" : "&" + cardSuit.toLowerCase() + ";";
+
+  const card = document.createElement("div");
+  card.classList.add("card", cardSuit.toLowerCase());
+  card.innerHTML =
+    '<span class="card-value-suit-top">' +
+    cardBody +
+    "</span>" +
+    '<span class="card-value-center">' +
+    cardValue +
+    "</span>" +
+    '<span class="card-value-suit-bot">' +
+    cardBody +
+    "</span>" +
+    "</span>";
+  document.body.appendChild(card);
+}
+
+const cards = deckBuilder();
+randomCard(cards);
