708557d7e6c64ce423177d21df68906a474a6ac3
dimaredunlap <dimaredunlap@gmail.com>
2022-06-28 17:25:08 +0000

    added js and css

--- src/app.js
+++ src/app.js
@@ -5,8 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function deckBuilder() {
-  const values = [
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  //   let numb = Math.floor(Math.random() * 12);
+  let numbers = [
     "A",
     "2",
     "3",
@@ -21,45 +27,12 @@ function deckBuilder() {
     "Q",
     "K"
   ];
-  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
-
-  // Next loop through the suits array whilst also looping through the values array and push the combined data into the cards array:
-
-  const cards = [];
-  for (let s = 0; s < suits.length; s++) {
-    for (let v = 0; v < values.length; v++) {
-      const value = values[v];
-      const suit = suits[s];
-      cards.push({ value, suit });
-    }
-  }
-  return cards;
-}
-// a esta funcion le pasaremos los arrays de cards
-
-function randomCard(cards) {
-  const random = Math.floor(Math.random() * 51);
-  const cardValue = cards[random].value;
-  const cardSuit = cards[random].suit;
-
-  const cardBody =
-    cardSuit === "Diamonds" ? "&diams;" : "&" + cardSuit.toLowerCase() + ";";
-
-  const card = document.createElement("div");
-  card.classList.add("card", cardSuit.toLowerCase());
-  card.innerHTML =
-    '<span class="card-value-suit-top">' +
-    cardBody +
-    "</span>" +
-    '<span class="card-value-center">' +
-    cardValue +
-    "</span>" +
-    '<span class="card-value-suit-bot">' +
-    cardBody +
-    "</span>" +
-    "</span>";
-  document.body.appendChild(card);
-}
-
-const cards = deckBuilder();
-randomCard(cards);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  //   let numb = Math.floor(Math.random() * 12);
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
